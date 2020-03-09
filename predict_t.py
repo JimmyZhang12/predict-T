c4107c7ba0c7c2170ea3f15b4c352d1476e15fa2
Andrew <atsmith3@illinois.edu>
2020-03-08 19:46:53 -0500

    automation: Docker container
    Added a docker container to be able to build gem5, build mcpat and run the
    current version of the predict-T framework. ToDo still needs to be able to
    build and run the cadence tools. Maybe this script should be converted to use
    centos as the base image.

--- predict_t.py
+++ predict_t.py
@@ -50,30 +50,30 @@ class Benchmark:
     self.gem5_out = gem5_out
     self.mcpat_out = mcpat_out
 
-#benchmarks = [Benchmark("basicmath", "basicmath_small", "", "text_out/basicmath", "gem5_out/basicmath", "mcpat_out/basicmath"),
-#              Benchmark("bitcnts", "bitcnts", "10000", "text_out/bitcnts", "gem5_out/bitcnts", "mcpat_out/bitcnts"),
-#              Benchmark("qsort_small", "qsort_small", "input/qsort_small.dat", "text_out/qsort_small", "gem5_out/qsort_small", "mcpat_out/qsort_small"),
-#              Benchmark("qsort_large", "qsort_large", "input/qsort_large.dat", "text_out/qsort_large", "gem5_out/qsort_large", "mcpat_out/qsort_large"),
-#              Benchmark("susan_small_s", "susan", "input/susan_small.pgm output/susan_small_s.pgm -s", "text_out/susan_small_s", "gem5_out/susan_small_s", "mcpat_out/susan_small_s"),
-#              Benchmark("susan_small_e", "susan", "input/susan_small.pgm output/susan_small_e.pgm -e", "text_out/susan_small_e", "gem5_out/susan_small_e", "mcpat_out/susan_small_e"),
-#              Benchmark("susan_small_c", "susan", "input/susan_small.pgm output/susan_small_c.pgm -c", "text_out/susan_small_c", "gem5_out/susan_small_c", "mcpat_out/susan_small_c"),
-#              Benchmark("susan_large_s", "susan", "input/susan_large.pgm output/susan_large_s.pgm -s", "text_out/susan_large_s", "gem5_out/susan_large_s", "mcpat_out/susan_large_s"),
-#              Benchmark("susan_large_e", "susan", "input/susan_large.pgm output/susan_large_e.pgm -e", "text_out/susan_large_e", "gem5_out/susan_large_e", "mcpat_out/susan_large_e"),
-#              Benchmark("susan_large_c", "susan", "input/susan_large.pgm output/susan_large_c.pgm -c", "text_out/susan_large_c", "gem5_out/susan_large_c", "mcpat_out/susan_large_c"),
-#              Benchmark("dijkstra_small", "dijkstra_small", "input/dijkstra.dat", "text_out/dijkstra_small", "gem5_out/dijkstra_small", "mcpat_out/dijkstra_small"),
-#              Benchmark("dijkstra_large", "dijkstra_large", "input/dijkstra.dat", "text_out/dijkstra_large", "gem5_out/dijkstra_large", "mcpat_out/dijkstra_large"),
-#              Benchmark("patricia_small", "patricia", "input/patricia_small.udp", "text_out/patricia_small", "gem5_out/patricia_small", "mcpat_out/patricia_small"),
-#              Benchmark("blowfish_e", "blowfish", "e input/blowfish_small.asc output/blowfish_small.enc 1234567890abcdeffedcba0987654321", "text_out/blowfish_e", "gem5_out/blowfish_e", "mcpat_out/blowfish_e"),
-#              Benchmark("blowfish_d", "blowfish", "d input/blowfish_small.enc output/blowfish_small.asc 1234567890abcdeffedcba0987654321", "text_out/blowfish_d", "gem5_out/blowfish_d", "mcpat_out/blowfish_d"),
-#              Benchmark("rijndael_e", "rijndael", "input/rijndael_small.asc output/rijndael_small.enc e 1234567890abcdeffedcba09876543211234567890abcdeffedcba0987654321", "text_out/rijndael_e", "gem5_out/rijndael_e", "mcpat_out/rijndael_e"),
-#              Benchmark("rijndael_d", "rijndael", "input/rijndael_small.enc output/rijndael_small.asc d 1234567890abcdeffedcba09876543211234567890abcdeffedcba0987654321", "text_out/rijndael_d", "gem5_out/rijndael_d", "mcpat_out/rijndael_d"),
-#              Benchmark("sha", "sha", "input/sha_small.asc", "text_out/sha", "gem5_out/sha", "mcpat_out/sha"),
-#              Benchmark("crc", "crc", "input/crc_small.pcm", "text_out/crc", "gem5_out/crc", "mcpat_out/crc"),
-#              Benchmark("fft", "fft", "4 4096", "text_out/fft", "gem5_out/fft", "mcpat_out/fft"),
-#              Benchmark("fft_i", "fft", "4 8192 -i", "text_out/fft_i", "gem5_out/fft_i", "mcpat_out/fft_i"),
-#              Benchmark("toast", "toast", "-fps -c input/toast_small.au", "text_out/toast", "gem5_out/toast", "mcpat_out/toast"),
-#              Benchmark("untoast", "untoast", "-fps -c input/untoast_small.gsm", "text_out/untoast", "gem5_out/untoast", "mcpat_out/untoast")]
-benchmarks = [Benchmark("untoast", "untoast", "-fps -c input/toast_small.gsm", "text_out/untoast", "gem5_out/untoast", "mcpat_out/untoast")]
+benchmarks = [Benchmark("basicmath", "basicmath_small", "", "text_out/basicmath", "gem5_out/basicmath", "mcpat_out/basicmath"),
+              Benchmark("bitcnts", "bitcnts", "10000", "text_out/bitcnts", "gem5_out/bitcnts", "mcpat_out/bitcnts"),
+              Benchmark("qsort_small", "qsort_small", "input/qsort_small.dat", "text_out/qsort_small", "gem5_out/qsort_small", "mcpat_out/qsort_small"),
+              Benchmark("qsort_large", "qsort_large", "input/qsort_large.dat", "text_out/qsort_large", "gem5_out/qsort_large", "mcpat_out/qsort_large"),
+              Benchmark("susan_small_s", "susan", "input/susan_small.pgm output/susan_small_s.pgm -s", "text_out/susan_small_s", "gem5_out/susan_small_s", "mcpat_out/susan_small_s"),
+              Benchmark("susan_small_e", "susan", "input/susan_small.pgm output/susan_small_e.pgm -e", "text_out/susan_small_e", "gem5_out/susan_small_e", "mcpat_out/susan_small_e"),
+              Benchmark("susan_small_c", "susan", "input/susan_small.pgm output/susan_small_c.pgm -c", "text_out/susan_small_c", "gem5_out/susan_small_c", "mcpat_out/susan_small_c"),
+              Benchmark("susan_large_s", "susan", "input/susan_large.pgm output/susan_large_s.pgm -s", "text_out/susan_large_s", "gem5_out/susan_large_s", "mcpat_out/susan_large_s"),
+              Benchmark("susan_large_e", "susan", "input/susan_large.pgm output/susan_large_e.pgm -e", "text_out/susan_large_e", "gem5_out/susan_large_e", "mcpat_out/susan_large_e"),
+              Benchmark("susan_large_c", "susan", "input/susan_large.pgm output/susan_large_c.pgm -c", "text_out/susan_large_c", "gem5_out/susan_large_c", "mcpat_out/susan_large_c"),
+              Benchmark("dijkstra_small", "dijkstra_small", "input/dijkstra.dat", "text_out/dijkstra_small", "gem5_out/dijkstra_small", "mcpat_out/dijkstra_small"),
+              Benchmark("dijkstra_large", "dijkstra_large", "input/dijkstra.dat", "text_out/dijkstra_large", "gem5_out/dijkstra_large", "mcpat_out/dijkstra_large"),
+              Benchmark("patricia_small", "patricia", "input/patricia_small.udp", "text_out/patricia_small", "gem5_out/patricia_small", "mcpat_out/patricia_small"),
+              Benchmark("blowfish_e", "blowfish", "e input/blowfish_small.asc output/blowfish_small.enc 1234567890abcdeffedcba0987654321", "text_out/blowfish_e", "gem5_out/blowfish_e", "mcpat_out/blowfish_e"),
+              Benchmark("blowfish_d", "blowfish", "d input/blowfish_small.enc output/blowfish_small.asc 1234567890abcdeffedcba0987654321", "text_out/blowfish_d", "gem5_out/blowfish_d", "mcpat_out/blowfish_d"),
+              Benchmark("rijndael_e", "rijndael", "input/rijndael_small.asc output/rijndael_small.enc e 1234567890abcdeffedcba09876543211234567890abcdeffedcba0987654321", "text_out/rijndael_e", "gem5_out/rijndael_e", "mcpat_out/rijndael_e"),
+              Benchmark("rijndael_d", "rijndael", "input/rijndael_small.enc output/rijndael_small.asc d 1234567890abcdeffedcba09876543211234567890abcdeffedcba0987654321", "text_out/rijndael_d", "gem5_out/rijndael_d", "mcpat_out/rijndael_d"),
+              Benchmark("sha", "sha", "input/sha_small.asc", "text_out/sha", "gem5_out/sha", "mcpat_out/sha"),
+              Benchmark("crc", "crc", "input/crc_small.pcm", "text_out/crc", "gem5_out/crc", "mcpat_out/crc"),
+              Benchmark("fft", "fft", "4 4096", "text_out/fft", "gem5_out/fft", "mcpat_out/fft"),
+              Benchmark("fft_i", "fft", "4 8192 -i", "text_out/fft_i", "gem5_out/fft_i", "mcpat_out/fft_i"),
+              Benchmark("toast", "toast", "-fps -c input/toast_small.au", "text_out/toast", "gem5_out/toast", "mcpat_out/toast"),
+              Benchmark("untoast", "untoast", "-fps -c input/untoast_small.gsm", "text_out/untoast", "gem5_out/untoast", "mcpat_out/untoast")]
+#benchmarks = [Benchmark("untoast", "untoast", "-fps -c input/toast_small.gsm", "text_out/untoast", "gem5_out/untoast", "mcpat_out/untoast")]
 #benchmarks = [Benchmark("susan_large_s", "susan", "input/susan_large.pgm output/susan_large_s.pgm -s", "text_out/susan_large_s", "gem5_out/susan_large_s", "mcpat_out/susan_large_s"),
 #              Benchmark("susan_large_e", "susan", "input/susan_large.pgm output/susan_large_e.pgm -e", "text_out/susan_large_e", "gem5_out/susan_large_e", "mcpat_out/susan_large_e"),
 #              Benchmark("susan_large_c", "susan", "input/susan_large.pgm output/susan_large_c.pgm -c", "text_out/susan_large_c", "gem5_out/susan_large_c", "mcpat_out/susan_large_c"),
@@ -88,22 +88,22 @@ def run(args):
       run_gem5(test.cmd,test.opt,test.text_out,test.gem5_out,"1","DerivO3CPU","16kB","64kB","256kB", mcpat_template, mcpat_path, mcpat_out, test.name)
 
   # First run Gem5 for each benchmark:
-  for test in benchmarks:
-    run_gem5(test.cmd,test.opt,test.text_out,test.gem5_out,"1","DerivO3CPU","16kB","64kB","256kB", mcpat_template, mcpat_path, mcpat_out, test.name)
-
-  #input = Queue()
-  #threads = []
-
   #for test in benchmarks:
-  #  input.put(test)
+  #  run_gem5(test.cmd,test.opt,test.text_out,test.gem5_out,"1","DerivO3CPU","16kB","64kB","256kB", mcpat_template, mcpat_path, mcpat_out, test.name)
 
-  #for i in range(16):
-  #  thr = Thread(target=m5_benchmark_thread, args=[input])
-  #  thr.start()
-  #  threads.append(thr)
+  input = Queue()
+  threads = []
 
-  #for thr in threads:
-  #  thr.join()
+  for test in benchmarks:
+    input.put(test)
+
+  for i in range(16):
+    thr = Thread(target=m5_benchmark_thread, args=[input])
+    thr.start()
+    threads.append(thr)
+
+  for thr in threads:
+    thr.join()
 
   # Convert Output to McPat:
   #for test in benchmarks:
