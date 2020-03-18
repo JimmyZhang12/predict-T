1cdae7f1a8e1a66d5d61ee87b222f500365675be
Andrew <atsmith3@illinois.edu>
2020-03-18 18:59:20 -0500

    cleaning: Preparing to Merge to Master
    Removing submodules and directories that are no longer needed.

--- verilog_sim/hello.c
+++
@@ -1,7 +0,0 @@
-#include "hello.h"
-#include <stdio.h>
-
-void hello_world(int a) {
-  printf("Hello World from C: %d\n", a);
-  hello_world_v(a);
-}
--- verilog_sim/hello.h
+++
@@ -1,9 +0,0 @@
-#ifndef HELLO_WORLD_H
-#define HELLO_WORLD_H
-
-#include "svdpi.h"
-#include "Vtop__Dpi.h"
-
-void hello_world(int a);
-
-#endif
