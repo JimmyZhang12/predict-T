04aed86bc4724eea2ae1cc09691be6f2f592361a
Andrew <atsmith3@illinois.edu>
2020-03-25 14:55:47 -0500

    benchmarks: Added a microbenchmarking suite
    Added benchmarks to verify the correctness of the MCPAT/GEM5. There are tests
    for INTADD, INTMUL, DADD, DMUL, MEM_HIERARCHY, IDLE, PHASES, CACHE_COHERENCE

---
+++ mcpat_bench/build.sh
@@ -0,0 +1,8 @@
+g++ mb.cc -std=c++11 -DNTHREADS=$1 -DINT_ADD_TEST -lpthread -O0 -o mb_int_add
+g++ mb.cc -std=c++11 -DNTHREADS=$1 -DINT_MUL_TEST -lpthread -O0 -o mb_int_mul
+g++ mb.cc -std=c++11 -DNTHREADS=$1 -DDOUBLE_ADD_TEST -lpthread -O0 -o mb_double_add
+g++ mb.cc -std=c++11 -DNTHREADS=$1 -DDOUBLE_ADD_TEST -lpthread -O0 -o mb_double_mul
+g++ mb.cc -std=c++11 -DNTHREADS=$1 -DMEM -lpthread -O0 -o mb_mem
+g++ mb.cc -std=c++11 -DNTHREADS=$1 -DSLEEP -lpthread -O0 -o mb_sleep
+g++ mb.cc -std=c++11 -DNTHREADS=$1 -DPHASES -lpthread -O0 -o mb_phases
+g++ mb.cc -std=c++11 -DNTHREADS=$1 -DCACHE_COHERENCE -lpthread -O0 -o mb_cache_coherence
