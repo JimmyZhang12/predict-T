88b7b3752f2f02a0c044d45f4093023ded6bb139
Andrew <atsmith3@illinois.edu>
2020-03-31 09:27:25 -0500

    power_supply_interface: Changed scripts to use 64 bit
    Pointed to the 64 bit version of NCVerilog and adjusted the compiler flags for
    all the scripts to build 64 bit so libraries.

--- power_supply_model/build.sh
+++ power_supply_model/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-g++ -m32 -Wall -shared -std=c++11 -DINTERPROCESS_PYTHON -D_XOPEN_SOURCE=500 -fPIC `python3 -m pybind11 --includes` interprocess.cpp -o interprocess`python3-config --extension-suffix` -lpthread -lrt 
+g++ -Wall -shared -std=c++11 -DINTERPROCESS_PYTHON -D_XOPEN_SOURCE=500 -fPIC `python3 -m pybind11 --includes` interprocess.cpp -o interprocess`python3-config --extension-suffix` -lpthread -lrt 
+#gcc -Wall -shared -std=c11 -D_XOPEN_SOURCE=500 -fPIC interprocess.c -o libinterprocess.so -lpthread -lrt 
--- power_supply_model/run.py
+++ power_supply_model/run.py
@@ -1,9 +1,13 @@
 import vsim
 import sys
 
-vsim.initialize("a3lhsdflj234jf", 100)
-with open("../test_load.csv", "r") as profile:
+vsim.initialize("a3lhsdflj234jf", 250)
+# Warmup Period:
+with open("mb_mem_500_16kB_64kB_256kB_32MB.csv", "r") as profile:
   data = profile.readlines()
+  for i in range(400): # 100us Warmup
+    vsim.set_driver_signals(1.0, float(data[0].split(",")[1].strip()), 0)
+    vsim.get_voltage()
   for point in data:
     vsim.set_driver_signals(1.0, float(point.split(",")[1].strip()), 0)
     vsim.get_voltage()
