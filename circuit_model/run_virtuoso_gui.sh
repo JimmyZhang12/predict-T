d35dcd4c42b9e06dbce07449c8ed4286ae5542b2
Andrew Smith <atsmith3@illinois.edu>
2020-05-26 21:58:42 -0500

    attemt to run from ews, exodus is not cooperating

--- circuit_model/run_virtuoso_gui.sh
+++ circuit_model/run_virtuoso_gui.sh
@@ -36,37 +36,4 @@ export LM_LICENSE_FILE=5280@cadence.webstore.illinois.edu
 # So far only CentOS 7 with GLIBC version 2.17 has worked.
 export OA_UNSUPPORTED_PLAT=linux_rhel50_gcc48x
 
-pushd /run_vsim
-cp $SIM_ROOT/interprocess.cpp ./interprocess.c
-cp $SIM_ROOT/interprocess.h .
-cp $SIM_ROOT/common/* .
-cp $SIM_ROOT/pdn/* .
-cp $SIM_ROOT/supply/* .
-cp $SIM_ROOT/circuit_model.vams .
-cp $SIM_ROOT/scf.scs .
-
-# BUILD 64 Bit VPI Library:
-gcc interprocess.c -O0 -g -DWITH_VPI -std=c11 -D_XOPEN_SOURCE=500 -fPIC -fpermissive -shared -I/software/cadence-Aug2016/INCISIVE152/tools.lnx86/include -lpthread -o interprocess.so
-
-# RUN 64 Bit version of VSIM:
-# TODO: Update this portion of the script to use the IRUN Wrapper with better flags.
-#+define+${3}=1 \
-#+define+${4}=1 \
-ncverilog \
-  +define+SHM_NAME=\\\"${1}\\\" \
-  +define+STEP_SIZE=${2} \
-  circuit_model.vams \
-  +access+r -loadvpi ./interprocess.so:register_create_shm \
-  -loadvpi ./interprocess.so:register_destroy_shm \
-  -loadvpi ./interprocess.so:register_wait_driver_data \
-  -loadvpi ./interprocess.so:register_get_voltage_setpoint \
-  -loadvpi ./interprocess.so:register_get_effective_resistance \
-  -loadvpi ./interprocess.so:register_get_prediction \
-  -loadvpi ./interprocess.so:register_get_enable \
-  -loadvpi ./interprocess.so:register_get_terminate_simulation \
-  -loadvpi ./interprocess.so:register_ack_driver_data \
-  -loadvpi ./interprocess.so:register_send_voltage \
-  -loadvpi ./interprocess.so:register_send_current \
-  -loadvpi ./interprocess.so:register_ack_simulation \
-  -top circuit_model \
-  -analogcontrol scf.scs > "$OUTPUT_ROOT/circuit_model/log/${1}_out.log"
+virtuoso
--- runscript/run.sh
+++ runscript/run.sh
@@ -78,7 +78,7 @@ echo "[ run.sh ] INPUT $INPUT"
 #--------------------------------------------------------------------
 # Configure Simulation Parameters
 #--------------------------------------------------------------------
-DURATION=("10000000")
+DURATION=("100000000")
 INTERVAL=("1000000")
 STEP=("1000")
 PROFILE_START=("0")
@@ -88,15 +88,15 @@ L1I=("32kB")
 L2=("256kB")
 L3=("16MB")
 
-#name=("basicmath" "bitcnts" "qsort" "susan_smooth" "susan_edge" "susan_corner" "dijkstra" "patricia" "blowfish_encrypt" "blowfish_decrypt" "rijndael_encrypt" "rijndael_decrypt" "sha" "crc" "fft" "ffti" "toast" "untoast")
-#exe=("basicmath" "bitcnts" "qsort" "susan" "susan" "susan" "dijkstra" "patricia" "blowfish" "blowfish" "rijndael" "rijndael" "sha" "crc" "fft" "fft" "toast" "untoast")
-#opt=("" "1000" "${INPUT}/qsort_large.dat" "${INPUT}/susan_large.pgm ${output}/susan_large_s.pgm -s" "${INPUT}/susan_large.pgm ${output}/susan_large_s.pgm -e" "${INPUT}/susan_large.pgm ${output}/susan_large_s.pgm -c" "${INPUT}/dijkstra.dat" "${INPUT}/patricia_small.udp" "e ${INPUT}/blowfish_small.asc ${output}/blowfish_small.enc 1234567890abcdeffedcba0987654321" "d ${INPUT}/blowfish_small.enc ${output}/blowfish_small.asc 1234567890abcdeffedcba0987654321" "${INPUT}/rijndael_small.asc ${output}/rijndael_small.enc e 1234567890abcdeffedcba09876543211234567890abcdeffedcba0987654321" "${INPUT}/rijndael_small.enc ${output}/rijndael_small.asc d 1234567890abcdeffedcba09876543211234567890abcdeffedcba0987654321" "${INPUT}/sha_small.asc" "${INPUT}/crc_small.pcm" "4 4096" "4 8192 -i" "-fps -c ${INPUT}/toast_small.au" "-fps -c ${INPUT}/toast_small.gsm")
+name=("basicmath" "bitcnts" "qsort" "susan_smooth" "susan_edge" "susan_corner" "dijkstra" "patricia" "blowfish_encrypt" "blowfish_decrypt" "rijndael_encrypt" "rijndael_decrypt" "sha" "crc" "fft" "ffti" "toast" "untoast")
+exe=("basicmath" "bitcnts" "qsort" "susan" "susan" "susan" "dijkstra" "patricia" "blowfish" "blowfish" "rijndael" "rijndael" "sha" "crc" "fft" "fft" "toast" "untoast")
+opt=("" "1000" "${INPUT}/qsort_large.dat" "${INPUT}/susan_large.pgm ${output}/susan_large_s.pgm -s" "${INPUT}/susan_large.pgm ${output}/susan_large_s.pgm -e" "${INPUT}/susan_large.pgm ${output}/susan_large_s.pgm -c" "${INPUT}/dijkstra.dat" "${INPUT}/patricia_small.udp" "e ${INPUT}/blowfish_small.asc ${output}/blowfish_small.enc 1234567890abcdeffedcba0987654321" "d ${INPUT}/blowfish_small.enc ${output}/blowfish_small.asc 1234567890abcdeffedcba0987654321" "${INPUT}/rijndael_small.asc ${output}/rijndael_small.enc e 1234567890abcdeffedcba09876543211234567890abcdeffedcba0987654321" "${INPUT}/rijndael_small.enc ${output}/rijndael_small.asc d 1234567890abcdeffedcba09876543211234567890abcdeffedcba0987654321" "${INPUT}/sha_small.asc" "${INPUT}/crc_small.pcm" "4 4096" "4 8192 -i" "-fps -c ${INPUT}/toast_small.au" "-fps -c ${INPUT}/toast_small.gsm")
 #name=("dijkstra" "toast" "susan_smooth" "fft")
 #exe=("dijkstra" "toast" "susan" "fft")
 #opt=("${INPUT}/dijkstra.dat" "-fps -c ${INPUT}/toast_small.au" "${INPUT}/susan_large.pgm ${output}/susan_large_s.pgm -s" "4 4096")
-name=("dijkstra")
-exe=("dijkstra")
-opt=("${INPUT}/dijkstra.dat")
+#name=("dijkstra")
+#exe=("dijkstra")
+#opt=("${INPUT}/dijkstra.dat")
 
 #TABLE_SIZE=("1024" "4096")
 #PC_START=("6" "10" "14")
@@ -119,85 +119,10 @@ for j in ${!name[@]}; do
         for m in ${!PC_START[@]}; do
           for o in ${!HISTORY_SIZE[@]}; do
             #TN="${name[$j]}_${TABLE_SIZE[$l]}_${PC_START[$m]}_${INTERVAL[$i]}_${HISTORY_SIZE[$o]}_SimplePredictorDisableBuck1MHz"
-            TN="${name[$j]}_${TABLE_SIZE[$l]}_${PC_START[$m]}_${INTERVAL[$i]}_SimplePredictorEnableBuck1MHz"
+            TN="${name[$j]}_${TABLE_SIZE[$l]}_${PC_START[$m]}_${INTERVAL[$i]}_harvard_PDN_Test"
 #            TN="${name[$j]}_${TABLE_SIZE[$l]}_${PC_START[$m]}_${INTERVAL[$i]}_mcSimplePredictorEnableBuck1MHz"
             se_single_core_xeon_e7_8893 $TN ${DURATION[$i]} ${INTERVAL[$i]} ${STEP[$i]} ${PROFILE_START[$i]} ${exe[$j]} "${opt[$j]}"
 #            se_n_core_xeon_e7_8893 $TN ${DURATION[$i]} ${INTERVAL[$i]} ${STEP[$i]} ${PROFILE_START[$i]} ${exe[$j]} "${opt[$j]}" "4"
-#--debug-flags=SimpleHistoryPowerPred,PowerPred,StatEvent \
-  #--debug-flags=SimpleHistoryPowerPred \
-  #--debug-flags=TestPowerPred,PowerPred,StatEvent \
-#--power_pred_type=SimpleHistoryPowerPredictor \
-#      echo "
-#../gem5/build/X86/gem5.opt \
-#--outdir=${OUTPUT_ROOT}/gem5_out/$TN \
-#--mcpat_template=${PREDICT_T_ROOT}/mcpat-template-x86-sc.xml \
-#--mcpat_path=${PREDICT_T_ROOT}/mcpat \
-#--mcpat_out=${OUTPUT_ROOT}/mcpat_out \
-#--mcpat_testname=$TN \
-#--power_profile_start=${PROFILE_START[$i]} \
-#--power_profile_duration=${DURATION[$i]} \
-#--power_profile_interval=${INTERVAL[$i]} \
-#--ncverilog_path=${PREDICT_T_ROOT}/package_model \
-#--ncverilog_step=${STEP[$i]} \
-#../gem5/configs/example/se.py \
-#--cmd=testbin/${exe[$j]} \
-#--opt=\"${opt[$j]}\" \
-#--power_profile_interval=${INTERVAL[$i]} \
-#--power_pred_type=TestPowerPredictor \
-#--power_pred_table_size=${TABLE_SIZE[$l]} \
-#--power_pred_pc_start=${PC_START[$m]} \
-#--power_pred_history_size=${HISTORY_SIZE[$o]} \
-#--num-cpus=1 \
-#--cpu-type=DerivO3CPU \
-#--l1i_size=${L1I[$k]} \
-#--l1i-hwp-type=TaggedPrefetcher \
-#--l1d_size=${L1D[$k]} \
-#--l1d-hwp-type=TaggedPrefetcher \
-#--l2cache \
-#--num-l2caches=1 \
-#--l2_size=${L2[$k]} \
-#--l2-hwp-type=TaggedPrefetcher \
-#--l3cache \
-#--l3_size=${L3[$k]} \
-#--l3-hwp-type=TaggedPrefetcher \
-#--caches \
-#--sys-clock=3.5GHz \
-#--mem-size=8GB > ${OUTPUT_ROOT}text_out/$TN.out &"
-#../gem5/build/X86/gem5.opt \
-#  --outdir=${OUTPUT_ROOT}/gem5_out/$TN \
-#  --mcpat_template=${PREDICT_T_ROOT}/mcpat-template-x86-sc.xml \
-#  --mcpat_path=${PREDICT_T_ROOT}/mcpat \
-#  --mcpat_out=${OUTPUT_ROOT}/mcpat_out \
-#  --mcpat_testname=$TN \
-#  --power_profile_start=${PROFILE_START[$i]} \
-#  --power_profile_duration=${DURATION[$i]} \
-#  --power_profile_interval=${INTERVAL[$i]} \
-#  --ncverilog_path=${PREDICT_T_ROOT}/package_model \
-#  --ncverilog_step=${STEP[$i]} \
-#  ../gem5/configs/example/se.py \
-#  --cmd=testbin/${exe[$j]} \
-#  --opt="${opt[$j]}" \
-#  --power_profile_interval=${INTERVAL[$i]} \
-#  --power_pred_type=TestPowerPredictor \
-#  --power_pred_table_size=${TABLE_SIZE[$l]} \
-#  --power_pred_pc_start=${PC_START[$m]} \
-#  --power_pred_history_size=${HISTORY_SIZE[$o]} \
-#  --num-cpus=1 \
-#  --cpu-type=DerivO3CPU \
-#  --l1i_size=${L1I[$k]} \
-#  --l1i-hwp-type=TaggedPrefetcher \
-#  --l1d_size=${L1D[$k]} \
-#  --l1d-hwp-type=TaggedPrefetcher \
-#  --l2cache \
-#  --num-l2caches=1 \
-#  --l2_size=${L2[$k]} \
-#  --l2-hwp-type=TaggedPrefetcher \
-#  --l3cache \
-#  --l3_size=${L3[$k]} \
-#  --l3-hwp-type=TaggedPrefetcher \
-#  --caches \
-#  --sys-clock=3.5GHz \
-#  --mem-size=8GB > ${OUTPUT_ROOT}/text_out/$TN.out &
             while [ `jobs | wc -l` -ge 32 ]; do
               sleep 1
             done
